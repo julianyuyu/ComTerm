25b51a0aa918804d4a077f8450b772ff93c36faf
Julian Yu <juy@microsoft.com>
2020-06-09 12:56:45 +0800

    add device enumerating

--- DeviceManager.cs
+++ DeviceManager.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using static ComTerm.NativeAPI;
-
+using static ComTerm.NativeDeviceInterfaceAPI;
 namespace ComTerm
 {
     public class DeviceManager
@@ -24,10 +24,85 @@ namespace ComTerm
             return true;
         }
 
-        protected void EnumerateDevice(Guid DeviceGuid)
+        private Tuple<string, string> GetDeviceName(IntPtr hDevInfo, ref SP_DEVICE_INTERFACE_DATA interfaceData)
+        {
+            uint requiredSize = 0;
+            string deviceName = null;
+            string friendlyName = null;
+            SP_DEVINFO_DATA infoData = new SP_DEVINFO_DATA();
+            infoData.cbSize = Marshal.SizeOf(infoData);
+
+            //get size first
+            SetupDiGetDeviceInterfaceDetail(hDevInfo, ref interfaceData, IntPtr.Zero, 0, ref requiredSize, ref infoData /*infoData*/);
+            IntPtr detailDataBuffer = Marshal.AllocHGlobal((int)requiredSize);
+            if (detailDataBuffer != IntPtr.Zero)
+            {
+                // get dev name now.
+                SP_DEVICE_INTERFACE_DETAIL_DATA detailData = new SP_DEVICE_INTERFACE_DETAIL_DATA();
+                detailData.cbSize = Marshal.SizeOf(typeof(SP_DEVICE_INTERFACE_DETAIL_DATA));
+                Marshal.StructureToPtr(detailData, detailDataBuffer, false);
+
+                if (SetupDiGetDeviceInterfaceDetail(hDevInfo, ref interfaceData, detailDataBuffer, requiredSize, ref requiredSize, ref infoData))
+                {
+                    IntPtr pdevicePathName = (IntPtr)((int)detailDataBuffer + 4);
+                    deviceName = Marshal.PtrToStringAuto(pdevicePathName);
+
+                    uint regDataType = 0;
+
+                    // get size for friendly name
+                    SetupDiGetDeviceRegistryProperty(hDevInfo, ref infoData, SPDRP_FRIENDLYNAME,
+                        ref regDataType, IntPtr.Zero, 0, ref requiredSize);
+                    IntPtr nameBuffer = Marshal.AllocHGlobal((int)requiredSize);
+                    if (SetupDiGetDeviceRegistryProperty(hDevInfo, ref infoData, SPDRP_FRIENDLYNAME,
+                        ref regDataType, nameBuffer, requiredSize, ref requiredSize))
+                    {
+                        friendlyName = Marshal.PtrToStringAuto(nameBuffer);
+                    }
+                    Marshal.FreeHGlobal(nameBuffer);
+                    nameBuffer = IntPtr.Zero;
+                }
+                Marshal.FreeHGlobal(detailDataBuffer);
+                detailDataBuffer = IntPtr.Zero;
+            }
+            return new Tuple<string, string>(deviceName, friendlyName);
+        }
+
+        protected uint EnumerateDevice(Guid DeviceGuid)
         {
             DeviceNames.Clear();
             FriendlyNames.Clear();
+
+            uint HandleIndex = 0;
+
+            // We start at the "root" of the device tree and look for all
+            // devices that match the interface GUID of a disk
+            var hDevInfo = SetupDiGetClassDevs(ref DeviceGuid, IntPtr.Zero, IntPtr.Zero, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
+
+            if (hDevInfo != IntPtr.Zero)
+            {
+                // create a Device Interface Data structure
+                SP_DEVICE_INTERFACE_DATA DeviceInterfaceData = new SP_DEVICE_INTERFACE_DATA();
+                DeviceInterfaceData.cbSize = Marshal.SizeOf(DeviceInterfaceData);
+
+                // start the enumeration 
+                while (SetupDiEnumDeviceInterfaces(hDevInfo,
+                    IntPtr.Zero, ref DeviceGuid, HandleIndex, ref DeviceInterfaceData))
+                {
+                    var names = GetDeviceName(hDevInfo, ref DeviceInterfaceData);
+                    if (string.IsNullOrEmpty(names.Item1))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        DeviceNames.Add(names.Item1);
+                        FriendlyNames.Add(names.Item2);
+                        HandleIndex++;
+                    }
+                }
+                SetupDiDestroyDeviceInfoList(hDevInfo);
+            }
+            return HandleIndex;
         }
     }
 }
--- Native.cs
+++ Native.cs
@@ -19,4 +19,91 @@ namespace ComTerm
         public static Guid GUID_DEVINTERFACE_COMPORT = new Guid("86E0D1E0-8089-11D0-9CE4-08003E301F73");
 
     }
+
+    /// <summary>
+    /// native API for device interface
+    /// </summary>
+    [SuppressUnmanagedCodeSecurityAttribute]
+    internal static class NativeDeviceInterfaceAPI
+    {
+        [DllImport(@"setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        internal static extern bool SetupDiGetDeviceInterfaceDetail(
+            IntPtr deviceInfoSet,
+            ref SP_DEVICE_INTERFACE_DATA deviceInterfaceData,
+            IntPtr deviceInterfaceDetailData,
+            uint deviceInterfaceDetailDataSize,
+            ref uint requiredSize,
+            ref SP_DEVINFO_DATA deviceInfoData);
+
+        //Get device, return true
+        [DllImport(@"setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        internal static extern Boolean SetupDiEnumDeviceInterfaces(
+            IntPtr hDevInfo,
+            IntPtr devInfo,
+            ref Guid interfaceClassGuid,
+            uint memberIndex,
+            ref SP_DEVICE_INTERFACE_DATA deviceInterfaceData);
+
+        [DllImport(@"setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        internal static extern Boolean SetupDiEnumDeviceInfo(
+            IntPtr hDevInfo,
+            uint memberIndex,
+            ref SP_DEVINFO_DATA DeviceInfoData);
+
+        [DllImport("setupapi.dll", CharSet = CharSet.Auto)]
+        internal static extern IntPtr SetupDiGetClassDevs(ref Guid ClassGuid, IntPtr Enumerator, IntPtr HwndParent, uint Flags);
+
+        [DllImport("setupapi.dll", CharSet = CharSet.Auto)]
+        internal static extern Boolean SetupDiDestroyDeviceInfoList(IntPtr hDevInfo);
+
+        [DllImport("setupapi.dll", CharSet = CharSet.Auto)]
+        internal static extern Boolean SetupDiGetDeviceRegistryProperty(
+            IntPtr hDevInfo,
+            ref SP_DEVINFO_DATA devInfoData,
+            uint Property,
+            ref uint PropertyRegDataType,
+            IntPtr PropertyBuffer,
+            uint PropertyBufferSize,
+            ref uint RequiredSize);
+
+        [StructLayout(LayoutKind.Sequential, Pack = 2)]
+        public class SP_DEVICE_INTERFACE_DETAIL_DATA
+        {
+            public int cbSize = Marshal.SizeOf(typeof(SP_DEVICE_INTERFACE_DETAIL_DATA));
+            public short devicePath;
+        }
+
+        //[StructLayout(LayoutKind.Sequential)]
+        public struct SP_DEVICE_INTERFACE_DATA
+        {
+            public int cbSize;// = Marshal.SizeOf(typeof(SP_DEVICE_INTERFACE_DATA));
+            public Guid interfaceClassGuid;
+            public int flags;
+            public int reserved;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SP_DEVINFO_DATA
+        {
+            public int cbSize;// = Marshal.SizeOf(typeof(SP_DEVINFO_DATA));
+            public Guid classGuid;// = Guid.Empty; // temp
+            public int devInst;// = 0; // dumy
+            public int reserved;// = 0;
+        }
+
+        //
+        // Device Interface Get Class Flag
+        // used in SetupDiGetClassDevsW
+        // https://docs.microsoft.com/en-us/windows/desktop/api/setupapi/nf-setupapi-setupdigetclassdevsw
+        //
+
+        public static readonly uint DIGCF_DEFAULT = 0x1;
+        public static readonly uint DIGCF_PRESENT = 0x2;
+        public static readonly uint DIGCF_ALLCLASSES = 0x4;
+        public static readonly uint DIGCF_PROFILE = 0x8;
+        public static readonly uint DIGCF_DEVICEINTERFACE = 0x10;
+
+        //
+        public static readonly uint SPDRP_FRIENDLYNAME = 0x0000000C;  // FriendlyName (R/W)
+    }
 }
